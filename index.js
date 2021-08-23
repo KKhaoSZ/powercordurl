1231b9f2832111e20914541c2d03889ac4b6658e
Khaos1324 <charles15851@gmail.com>
2021-08-22 20:10:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const { Plugin } = require('powercord/entities');
 const { get } = require('powercord/http');
 const { command } = require('../pc-commands/commands/echo');
 
-module.exports = class Linkshortner extends Plugin {
+module.exports = class Tnyurl extends Plugin {
     startPlugin () {
         powercord.api.commands.registerCommand({
             command: "tnyurl",
-            aliases: [ "url"],
+            aliases: [ "url", "tinyurl", "link", "short"],
             description: "Lets you shrink urls using tnyurl",
             usage: "{c}tnyurl <url>",
             executor: this.linkshortneris.bind(this)
         });
     }
     pluginWillUnload() {
-        powercord.pluginWillUnload('Linkshortner')
+        powercord.pluginWillUnload('Tnyurl')
     }
     async linkshortneris(args) {
         const data = await get("http://tinyurl.com/api-create.php?url="+args[0]);
